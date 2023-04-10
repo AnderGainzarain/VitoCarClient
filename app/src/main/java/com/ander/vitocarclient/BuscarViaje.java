b69d7f6d9493ca05423ba29d5372d9619ff946fb
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-10 17:17:53 +0200

    started implementing the buscar viaje funcionalities

--- app/src/main/java/com/ander/vitocarclient/BuscarViaje.java
+++ app/src/main/java/com/ander/vitocarclient/BuscarViaje.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +16,16 @@ import android.view.ViewGroup;
  */
 public class BuscarViaje extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BuscarViaje() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuscarViaje.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BuscarViaje newInstance(String param1, String param2) {
-        BuscarViaje fragment = new BuscarViaje();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Spinner sOrigen;
+    private Spinner sDestino;
+    private String[] ciudades = {"Vitoria", "Donostia", "Bilbo"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        View rootView = inflater
+        sOrigen = rootView.
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buscar_viaje, container, false);
-    }
+
 }
