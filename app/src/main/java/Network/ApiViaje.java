db257f6b3c1947c5a1e52150eae78db7e3a5ae4e
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-11 13:34:10 +0200

    Apiviaje now returns a list

--- app/src/main/java/Network/ApiViaje.java
+++ app/src/main/java/Network/ApiViaje.java
@@ -1,10 +1,12 @@
 package Network;
 
+import java.util.List;
+
 import Model.Viaje;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiViaje {
     @GET("api/viajes")
-    Call<Viaje> getViajes();
+    Call<List<Viaje>> getViajes();
 }
