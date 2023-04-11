6591779151d006ac2bd69195f4a0d8e40630e335
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-11 13:42:04 +0200

    all the viajes are shown

--- app/src/main/java/com/ander/vitocarclient/Controller/VerViajes.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/VerViajes.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ander.vitocarclient.R;
 
@@ -26,20 +27,28 @@ public class VerViajes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_viajes);
+        // Bind the recycler view
         rv = findViewById(R.id.rvVerViajes);
         rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        showViajes();
     }
     public void showViajes(){
         Call<List<Viaje>> call = ApiClient.getClient().create(ApiViaje.class).getViajes();
         call.enqueue(new Callback<List<Viaje>>() {
             @Override
             public void onResponse(Call<List<Viaje>> call, Response<List<Viaje>> response) {
-
+                // Show the viajes in the recycler view
+                if(response.isSuccessful()){
+                    viajes=response.body();
+                    adapter = new ViajeAdapter(viajes,getApplicationContext());
+                    rv.setAdapter(adapter);
+                }
             }
 
             @Override
             public void onFailure(Call<List<Viaje>> call, Throwable t) {
-
+                // return an error message if there is an error
+                Toast.makeText(VerViajes.this,"Ha ocurrido un error de conexion", Toast.LENGTH_SHORT).show();
             }
         });
     }
