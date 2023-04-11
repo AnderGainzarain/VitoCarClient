098d4df636bc03f02887de96cfb24c8c473a2cd3
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-11 13:35:17 +0200

    getClient ahora es un metodo publico

--- app/src/main/java/Network/ApiClient.java
+++ app/src/main/java/Network/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit;
-    private static Retrofit getClient(){
+    public static Retrofit getClient(){
         String ip = "192.168.1.11";
         String puerto = "8080";
         retrofit = new Retrofit.Builder().baseUrl("http://" + ip + ":" + puerto)
