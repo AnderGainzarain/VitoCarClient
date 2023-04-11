e761aadcc9b028d0a595eedd7b1af6502d1dbebd
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-11 13:24:07 +0200

    finished implementing the adapter

--- app/src/main/java/Adapter/ViajeAdapter.java
+++ app/src/main/java/Adapter/ViajeAdapter.java
@@ -1,31 +1,62 @@
 package Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ander.vitocarclient.R;
+
+import java.util.List;
+
+import Model.Viaje;
+
 public class ViajeAdapter extends RecyclerView.Adapter<ViajeAdapter.ViewHolder>{
+    private List<Viaje> viajes;
+    private Context context;
+
+    public ViajeAdapter(List<Viaje> viajes, Context context) {
+        this.viajes = viajes;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_viaje,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.origen.setText(viajes.get(position).getOrigen());
+        holder.destino.setText(viajes.get(position).getDestino());
+        holder.fecha.setText(viajes.get(position).getFechaSalida());
+        holder.precio.setText(viajes.get(position).getPrecio());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return viajes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView origen;
+        private TextView destino;
+        private TextView fecha;
+        private TextView precio;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            origen = itemView.findViewById(R.id.origen);
+            destino = itemView.findViewById(R.id.destino);
+            fecha = itemView.findViewById(R.id.tvFechaSalida);
+            precio = itemView.findViewById(R.id.precio);
+
         }
     }
 }
