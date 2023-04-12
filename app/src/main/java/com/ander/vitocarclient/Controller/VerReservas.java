5c7cd155eca3c688f5570b56ab2810b3d92f5fd7
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-12 15:57:51 +0200

    ver reservas completely implemented

--- app/src/main/java/com/ander/vitocarclient/Controller/VerReservas.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/VerReservas.java
@@ -2,65 +2,78 @@ package com.ander.vitocarclient.Controller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ander.vitocarclient.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VerReservas#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+import Adapter.ViajeAdapter;
+import Model.Viaje;
+import Network.ApiClient;
+import Network.ApiUser;
+import Network.ApiViaje;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class VerReservas extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<Viaje> viajes;
+    private RecyclerView rv;
+    private ViajeAdapter adapter;
+    private int dni = 3333;
 
     public VerReservas() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VerReservas.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VerReservas newInstance(String param1, String param2) {
-        VerReservas fragment = new VerReservas();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ver_reservas, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        rv = view.findViewById(R.id.rvVerReservas);
+        rv.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        showReservas();
+    }
+    public void showReservas(){
+        Call<List<Viaje>> call = ApiClient.getClient().create(ApiViaje.class).getMisReservas(dni);
+        call.enqueue(new Callback<List<Viaje>>() {
+            @Override
+            public void onResponse(Call<List<Viaje>> call, Response<List<Viaje>> response) {
+                // Show the viajes in the recycler view
+                if(response.isSuccessful()){
+                        viajes=response.body();
+                        if(viajes.isEmpty()){
+                            Toast.makeText(getContext(),"No hay viajes reservados", Toast.LENGTH_LONG).show();
+                        }else{
+                            adapter = new ViajeAdapter(viajes,getContext());
+                            rv.setAdapter(adapter);
+                        }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Viaje>> call, Throwable t) {
+                // return an error message if there is an error
+                Toast.makeText(getContext(),"Ha ocurrido un error de conexion", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
