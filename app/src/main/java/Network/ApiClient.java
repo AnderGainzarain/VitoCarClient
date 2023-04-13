1df057f3b0d7a934a87a83a7c5fe0167817c20ff
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-13 12:49:52 +0200

    updated the parameter type for getViajeConcreto()

--- app/src/main/java/Network/ApiClient.java
+++ app/src/main/java/Network/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     private static Retrofit retrofit;
     public static Retrofit getClient(){
-        String ip = "192.168.1.11";
+        String ip = "172.20.10.4";
         String puerto = "8080";
         retrofit = new Retrofit.Builder().baseUrl("http://" + ip + ":" + puerto)
                 .addConverterFactory(GsonConverterFactory.create()).build();
--- app/src/main/java/Network/ApiViaje.java
+++ app/src/main/java/Network/ApiViaje.java
@@ -6,14 +6,13 @@ import Model.Viaje;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiViaje {
-    @GET("api/viajes")
-    Call<List<Viaje>> getViajes();
     @GET("api/viajes/{dni}/pasajero")
     Call<List<Viaje>> getMisReservas(@Path("dni") int dni);
     @GET("api/viajes/viajeConcreto")
-    Call<List<Viaje>> getViajeConcreto(@Path("origen") String origen,
-                                       @Path("destino") String destino,
-                                       @Path("fechaSalida") String fechaSalida);
+    Call<List<Viaje>> getViajeConcreto(@Query("origen") String origen,
+                                       @Query("destino") String destino,
+                                       @Query("fechaSalida") String fechaSalida);
 }
