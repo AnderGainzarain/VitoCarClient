7478451f652092346b06b40b48250d27114cf8f4
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-13 08:04:43 +0200

    Started implementing the search functionality

--- app/src/main/java/com/ander/vitocarclient/Controller/Buscar.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/Buscar.java
@@ -10,9 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.ander.vitocarclient.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +28,9 @@ public class Buscar extends Fragment {
 
     private Spinner sOrigen;
     private Spinner sDestino;
-    private String[] ciudades = {"Vitoria", "Donostia", "Bilbo"};
-
+    private final String[] ciudades = {"Vitoria", "Donostia", "Bilbo"};
+    private EditText fecha;
+    private Button buscar;
     public Buscar() {
         // Required empty public constructor
     }
@@ -41,10 +47,29 @@ public class Buscar extends Fragment {
         // Get the spinners from the xml
         sOrigen = view.findViewById(R.id.sOrigen);
         sDestino = view.findViewById(R.id.sDestino);
+        // get the date from the xml
+        fecha = view.findViewById(R.id.etFecha);
+        buscar = view.findViewById(R.id.btnBuscar);
         // Create the contents of the spinners
         ArrayAdapter<String> adaptador = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, ciudades);
         // Load the contents into the spinners
         sOrigen.setAdapter(adaptador);
         sDestino.setAdapter(adaptador);
+        view.setOnClickListener(Search);
     }
+    private View.OnClickListener Search = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            String origen = sOrigen.getSelectedItem().toString();
+            String destino = sDestino.getSelectedItem().toString();
+            String fechaSalida = fecha.getText().toString();
+            Map<String,String> data = new HashMap<>();
+            data.put("origen",origen);
+            data.put("destnino", destino);
+            data.put("fechaSalida", fechaSalida);
+
+
+        }
+    };
+
 }
