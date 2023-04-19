3d13d34c33b4f29e78f66c4f9b97bfa46e747fd0
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-19 16:13:47 +0200

    simplified the method

--- app/src/main/java/com/ander/vitocarclient/Controller/Uils/DateManager.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/Uils/DateManager.java
@@ -10,13 +10,10 @@ public class DateManager {
        // Get the current time
         LocalDateTime now = LocalDateTime.now();
         // Cast the fecha to a date type
-        LocalDateTime fecha = parseDate(date, "00:00:00");
+        LocalDateTime fecha = parseDate(date.replace("/","-"), "00:00:00");
         System.out.println(fecha + "\n" + now);
         //Check if the fecha has passed
-        if (fecha.isBefore(now)) {
-            return false;
-        }else
-            return true;
+        return !fecha.isBefore(now);
     }
 
 }
