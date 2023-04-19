d66d113ddf932149695944703d52ce957d406ff6
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-04-19 12:48:43 +0200

    the default date and time are the current ones

--- app/src/main/java/com/ander/vitocarclient/Controller/Uils/DateAndTimePickers.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/Uils/DateAndTimePickers.java
@@ -1,6 +1,7 @@
 package com.ander.vitocarclient.Controller.Uils;
 
 import android.app.DatePickerDialog;
+import android.app.LocaleConfig;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.view.View;
@@ -8,16 +9,18 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.time.LocalDateTime;
+
 public class DateAndTimePickers{
         public static void mostrarFecha(View v,Context context, EditText fecha){
-
+            LocalDateTime now = LocalDateTime.now();
             DatePickerDialog date = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
                 @Override
                 public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                     fecha.setText(year+"-"+ dateFormat(month+1) +"-"+dateFormat(dayOfMonth));
 
                 }
-            }, 2023, 01,28 );
+            }, now.getYear(), now.getMonthValue()-1,now.getDayOfMonth());
             date.show();
         }
         private static String dateFormat(int n){
@@ -25,12 +28,13 @@ public class DateAndTimePickers{
         }
 
         public static void mostrarHora(View v, Context context, EditText hora){
+            LocalDateTime now = LocalDateTime.now();
             TimePickerDialog time = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
                 @Override
                 public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                     hora.setText(dateFormat(hourOfDay)+":"+dateFormat(minute) + ":00");
                 }
-            }, 10, 15, true);
+            }, now.getHour(), now.getMinute(), true);
             time.show();
         }{
     }
