3b1cb5eac1bfc43f976717328457a9c8aca0c3c7
ander <ander.gainzarain.mtz@gmail.com>
2023-04-27 13:06:42 +0200

    changed the active user initialization

--- app/src/main/java/com/ander/vitocarclient/Model/ActiveUser.java
+++ app/src/main/java/com/ander/vitocarclient/Model/ActiveUser.java
@@ -14,24 +14,28 @@ import retrofit2.Response;
 
 public class ActiveUser {
     // Atributes
-    private static Integer dni;
-    private static int telefono;
-    private static String mail;
-    private static String nombre;
-    private static String apellido;
-    private static String foto;
-    private static String coche;
+    private Integer dni;
+    private int telefono;
+    private String mail;
+    private String nombre;
+    private String apellido;
+    private String foto;
+    private String coche;
     private static ActiveUser activeUser;
 
     //Constructor
-    private ActiveUser(Integer DNI, int telefono, String mail, String nombre, String apellido, String foto, String coche){
-        dni = DNI;
-        ActiveUser.telefono = telefono;
-        ActiveUser.mail = mail;
-        ActiveUser.nombre = nombre;
-        ActiveUser.apellido = apellido;
-        ActiveUser.foto = foto;
-        ActiveUser.coche = coche;
+    private ActiveUser(Integer dni, int telefono, String mail, String nombre, String apellido, String foto, String coche){
+        this.dni = dni;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.foto = foto;
+        this.coche = coche;
+    }
+
+    public static void initialize(User user) {
+        activeUser = new ActiveUser(user.getDni(),user.getTelefono(),user.getMail(),user.getNombre(),user.getApellido(),user.getFoto(),user.getCoche());
     }
 
     public Integer getDNI() {
@@ -63,30 +67,6 @@ public class ActiveUser {
     }
 
     public static ActiveUser getActiveUser() {
-        if(activeUser==null){
-            startUser();
-        }
-
         return activeUser;
-    }
-
-    private static void startUser(){
-        Call<User> call = ApiClient.getClient().create(ApiUser.class).getUser(1111);
-        call.enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
-                User user = response.body();
-                String c;
-                if(user.getCoche().equals("")){
-                    c = "No hay coche registrado";
-                }else{
-                    c = user.getCoche();
-                }
-                activeUser = new ActiveUser(user.getDni(),user.getTelefono(),user.getMail(),user.getNombre(),user.getApellido(),user.getFoto(),c);
-            }
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-            }
-        });
     }
 }
