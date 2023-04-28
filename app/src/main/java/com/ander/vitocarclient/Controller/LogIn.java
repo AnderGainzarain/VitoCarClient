00f1eff5526810957e31e6a6abe2450e1ac9db49
ander <ander.gainzarain.mtz@gmail.com>
2023-04-28 10:40:15 +0200

    Continued with the log in implementation

--- app/src/main/java/com/ander/vitocarclient/Controller/LogIn.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/LogIn.java
@@ -1,5 +1,6 @@
 package com.ander.vitocarclient.Controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,7 +21,6 @@ import com.ander.vitocarclient.Network.ApiClient;
 import com.ander.vitocarclient.Network.ApiUser;
 import com.ander.vitocarclient.R;
 import com.ander.vitocarclient.Vista.ToastControll;
-import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,8 +37,10 @@ public class LogIn extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
@@ -45,15 +48,61 @@ public class LogIn extends Fragment {
         mail = view.findViewById(R.id.etMailLogIn);
         password = view.findViewById(R.id.etPasswordLogIn);
         logIn = view.findViewById(R.id.btnLogIn);
-        String eMail = mail.getText().toString();
-        String pwd = password.getText().toString();
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logIn(eMail, pwd);
-
+                logIn(mail.getText().toString(), password.getText().toString());
             }
         });
+    }
+    private class LogInTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String eMail = params[0];
+            String pwd = params[1];
+
+            Call<User> call = ApiClient.getClient().create(ApiUser.class).getUserMail(eMail);
+            call.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
+                    User user = response.body();
+                    if(user==null){
+                        Toast.makeText(getContext(), ToastControll.mailIncorrecto(), Toast.LENGTH_LONG).show();
+                        mail.setText("");
+                        password.setText("");
+                        return;
+                    }
+                    if(!user.getContraseña().equals(pwd)){
+                        Toast.makeText(getContext(), ToastControll.pwdIncorrecto(), Toast.LENGTH_LONG).show();
+                        mail.setText("");
+                        password.setText("");
+                        return;
+                    }
+                    ActiveUser.initialize(user);
+                }
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    Toast.makeText(getContext(), ToastControll.getConectionErrorMsg(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+
+            // Handle the result of the asynchronous task here, such as showing a Toast message or navigating to a new activity
+            if (success) {
+                Toast.makeText(getContext(), "Log in successful", Toast.LENGTH_SHORT).show();
+                getParentFragmentManager().beginTransaction().replace(R.id.flMain, new Perfil()).commit();
+
+            } else {
+                Toast.makeText(getContext(), "Log in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void logIn(String eMail, String pwd){
