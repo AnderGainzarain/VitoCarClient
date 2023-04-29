d5d74e904a7e3a26a168f17739b358044b941ee0
ander <ander.gainzarain.mtz@gmail.com>
2023-04-29 15:18:52 +0200

    added new messages

--- app/src/main/java/com/ander/vitocarclient/Vista/TextControll.java
+++ app/src/main/java/com/ander/vitocarclient/Vista/TextControll.java
@@ -1,11 +1,9 @@
 package com.ander.vitocarclient.Vista;
 
 public class TextControll {
-    // Conection error
     public static String getConectionErrorMsg(){
         return "Ha ocurrido un error de conexion";
     }
-    // there are no viajes
     public static String noViajesPublicados(){
         return "No hay viajes";
     }
@@ -14,24 +12,18 @@ public class TextControll {
     public static String fechaVacia(){return "Introduzca una fecha, por favor";}
     public static String origenDestinoIguales(){return "El origen y el destino no pueden ser el mismo";}
     public static String precioMenorUno(){return "El coste no puede ser menor que 1€";}
-
     public static String precioVacio() {return "Por favor introduzca un precio";}
-
     public static String fechaPasada() {return "La fecha de salida ya ha pasado";}
-
     public static String viajePublicado() { return "Viaje publciado con exito";}
     public static String errorPublicar() {return "Error al publicar";}
-
     public static String mailIncorrecto() {return "El mail es incorrecto";}
-
     public static String pwdIncorrecto() {return "La contraseña es incorrecta";}
-
     public static String publicarNoLogueado() { return "Es necesario iniciar sesion para publicar un viaje";}
-
     public static String reservasNoLogueado() {return "Es necesario iniciar sesion para ver tus reservas";}
     public static String viajesNoLogueado() {return "Es necesario iniciar sesion para ver tus viajes publicados";}
     public static String viajeYaPublicado() { return "Ya has publicado un viaje con ese origen y destino para la misma hora";}
-
-
     public static String btnAnular() {return "anular";}
+    public static String reservaRealizada() {return "Reserva realizada con exito";}
+    public static String viajeAnulado() {return "Viaje anulado con exito";}
+    public static String reservaAnulada(){return "Reserva anulada con exito";}
 }
