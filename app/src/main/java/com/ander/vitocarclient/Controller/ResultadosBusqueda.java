0c299eb4775ff28e3717be27cc326a5bcfbbf7db
AnderGainzarain <ander.gainzarain.mtz@gmail.com>
2023-05-11 09:27:02 +0200

    updated the comments

--- app/src/main/java/com/ander/vitocarclient/Controller/ResultadosBusqueda.java
+++ app/src/main/java/com/ander/vitocarclient/Controller/ResultadosBusqueda.java
@@ -52,11 +52,14 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // retrieve the query data from the buscar fragment
         getParentFragmentManager().setFragmentResultListener("query", this, (requestKey, result) -> {
+            // store the query data in a class atribute
             queryData.put("origen",result.getString("origen"));
             queryData.put("destino", result.getString("destino"));
             queryData.put("fechaSalida",result.getString("fechaSalida"));
             queryData.put("horaSalida",result.getString("horaSalida"));
+            // start the process to show the found viajes
             misReservas();
         });
     }
@@ -68,10 +71,12 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
     }
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+        // bind the recycler view and create an adapter
         rv = view.findViewById(R.id.rvResultadosBusqueda);
         rv.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
     private void misReservas(){
+        // if the user is logged in get their reservas
         if(au!=null){
             Call<List<Viaje>> call = ApiClient.getClient().create(ApiViaje.class).getMisReservas(au.getDNI());
             call.enqueue(new Callback<List<Viaje>>() {
@@ -82,6 +87,7 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                         if(aux!=null){
                             misReservas = aux;
                         }
+                        // continue the process to show the viajes
                         misViajes();
                     }
                 }
@@ -90,10 +96,12 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                     Toast.makeText(getContext(), TextControll.getConectionErrorMsg() + t.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
+            // if the user is not logged in continue with the process to show the viajes
         }else misViajes();
 
     }
     private void misViajes(){
+        // if the user is logged in get the viajes they are offering
         if(au!=null){
             Call<List<Viaje>> call = ApiClient.getClient().create(ApiUser.class).getMisViajes(au.getDNI());
             call.enqueue(new Callback<List<Viaje>>() {
@@ -104,6 +112,7 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                         if(aux!=null){
                             misViajes = aux;
                         }
+                        // continue with the process to show the viajes
                         busqueda();
                     }
                 }
@@ -112,21 +121,24 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                     Toast.makeText(getContext(), TextControll.getConectionErrorMsg() + t.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
+            // continue with the process to show the viajes
         }else busqueda();
 
     }
     private void busqueda(){
+        // get all the viajes that match the query data
         Call<List<Viaje>> call = ApiClient.getClient().create(ApiViaje.class).getViajeConcreto(queryData.get("origen"),queryData.get("destino"), DateManager.parseDate(queryData.get("fechaSalida"),queryData.get("horaSalida")));
         call.enqueue(new Callback<List<Viaje>>() {
             @Override
             public void onResponse(@NonNull Call<List<Viaje>> call, @NonNull Response<List<Viaje>> response) {
-                // Show the viajes in the recycler view
                if(response.isSuccessful()){
                     viajes=response.body();
+                    // notify the user if there are not viajes that match the query data
                     if(viajes == null || viajes.isEmpty()){
                         Toast.makeText(getContext(), TextControll.noHayBusqueda(), Toast.LENGTH_SHORT).show();
                     }else{
                         if(au!=null){
+                            // if the user is logged in delete the viajes they have published
                             for(int i = 0; i <viajes.size();i++){
                                 for(int j = 0; j<misViajes.size();j++){
                                     if(viajes.get(i).getIdViaje()==misViajes.get(j).getIdViaje()){
@@ -134,6 +146,7 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                                     }
                                 }
                             }
+                            // if the user is logged in delete the viajes they have already booked
                             for(int i = 0; i <viajes.size();i++){
                                 for(int j = 0; j<misReservas.size();j++){
                                     if(viajes.get(i).getIdViaje()==misReservas.get(j).getIdViaje()){
@@ -142,13 +155,13 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
                                 }
                             }
                         }
+                        // Short the viajes and show them
                         viajes =viajes.stream().sorted(Comparator.comparing(Viaje::getFechaSalida)).collect(Collectors.toList());
                         adapter = new ViajeAdapter(viajes,ResultadosBusqueda.this);
                         rv.setAdapter(adapter);
                     }
                 }
             }
-
             @Override
             public void onFailure(@NonNull Call<List<Viaje>> call, @NonNull Throwable t) {
                 // return an error message if there is an error
@@ -159,12 +172,19 @@ public class ResultadosBusqueda extends Fragment implements RvInterface {
 
     @Override
     public void onItemClick(int position) {
+        // if the user is logged in allow a popup with more data to appear
         if(au!=null){
+            // Get the data of the selected viaje
             Viaje viaje = viajes.get(position);
+            // Show the pop up
             PopUpController.show(getContext(),R.layout.mas_info,getView());
+            // Show the data of the selected viaje
             PopUpController.showDataViaje(viaje.getOrigen(),viaje.getDestino(),viaje.getFechaSalida(),String.valueOf(viaje.getPrecio()));
+            // show the data of the driver of the viaje
             PopUpController.showDriverData(viaje.getIdViaje(), getContext());
+            // set the submit button text to reservar
             PopUpController.submitTextReservar();
+            // add an on click listener to the submit button to execute a reserva
             PopUpController.submitReservar(viaje.getIdViaje(), getContext());
         }
     }
